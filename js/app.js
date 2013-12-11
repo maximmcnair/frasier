b8a900d01b8b7aa66db4579df60a2b0b282c8398
Maxim McNair <mail@maximmcnair.com>
2013-12-11 17:51:03 +0000

    Added sortable to tasks

--- js/app.js
+++ js/app.js
@@ -7,7 +7,8 @@ angular.module('myApp', [
   'myApp.filters',
   'myApp.services',
   'myApp.directives',
-  'myApp.controllers'
+  'myApp.controllers',
+  'ui'
 ]).
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/',
