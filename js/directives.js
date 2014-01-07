1ccb702ea0ae1a282ef8291dae34b12e11483629
Maxim McNair <mail@maximmcnair.com>
2014-01-07 16:41:24 +0000

    Add directive to hide/show on checkbox

--- js/directives.js
+++ js/directives.js
@@ -3,9 +3,20 @@
 /* Directives */
 
 
-angular.module('myApp.directives', []).
-  directive('appVersion', ['version', function(version) {
+angular.module('myApp.directives', [])
+  .directive('appVersion', ['version', function (version) {
     return function(scope, elm, attrs) {
       elm.text(version);
-    };
-  }]);
+    }
+  }])
+  .directive('hideon', function() {
+    return function (scope, element, attrs) {
+      scope.$watch(attrs.hideon, function (value, oldValue) {
+        if(value) {
+          element.show()
+        } else {
+          element.hide()
+        }
+      }, true)
+    }
+  })
