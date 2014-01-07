8797a92516c2d6620cf62d21f44c0055064dfe3c
Maxim McNair <mail@maximmcnair.com>
2014-01-07 17:59:52 +0000

    Added back button directive

--- js/directives.js
+++ js/directives.js
@@ -20,3 +20,24 @@ angular.module('myApp.directives', [])
       }, true)
     }
   })
+.directive('backButton', function () {
+  return {
+    restrict: 'E',
+    template: '<button class="btn">{{back}}</button>',
+    scope: {
+      back: '@back',
+      forward: '@forward',
+      icons: '@icons'
+    },
+    link: function(scope, element, attrs) {
+      $(element[0]).on('click', function() {
+        history.back();
+        scope.$apply();
+      });
+      // $(element[1]).on('click', function() {
+      //   history.forward();
+      //   scope.$apply();
+      // });
+    }
+  };
+});
